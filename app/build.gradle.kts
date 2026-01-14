c24a589f2fa67bc23f4b5649b105b1a118f7bf75
mitko-donchev <mitko.a.donchev@gmail.com>
2026-01-14 23:04:18 +0200

    Create version 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.brax.apkstation"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
         setProperty("archivesBaseName", "ApkStation-$versionName")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
